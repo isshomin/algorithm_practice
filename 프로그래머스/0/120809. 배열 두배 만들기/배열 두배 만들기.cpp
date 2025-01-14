8701949c3e808f9baed594b2c57b16577b691f19
isshomin <79312705+isshomin@users.noreply.github.com>
2025-01-14 18:02:04 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.21 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+//     for(int i=0; i < numbers.size(); i++)
+//     {
+//         numbers[i] *= 2;
+//     }
+    
+//     answer = numbers;
+    for(auto &v : numbers)
+    {
+        v *= 2;
+    }
+    answer = numbers;
+    return answer;
+}
