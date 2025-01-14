d0b06e38ca872c52e7d0fb29f73261e7708611f2
isshomin <79312705+isshomin@users.noreply.github.com>
2025-01-14 17:46:11 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int odd = 0;
+    int even = 0;
+    for(int i=0; i < num_list.size(); i++)
+    {
+        if(num_list[i] % 2 == 1)
+        {
+            odd = odd * 10 + num_list[i];
+        }
+        else
+        {
+            even = even * 10 + num_list[i];
+        }
+    }
+    
+    answer = odd + even;
+    
+    return answer;
+}
