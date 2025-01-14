9595952b7ac433dcff3fb221d00a77499c84c48b
isshomin <79312705+isshomin@users.noreply.github.com>
2025-01-14 17:55:24 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    if(ineq == ">")
+    {
+        if(eq == "=")
+            return n >= m;
+        else if(eq == "!")
+            return n > m;
+    }
+    if(ineq == "<")
+    {
+        if(eq == "=")
+            return n <= m;
+        else if(eq == "!")
+            return n < m;
+    }
+    return answer;
+}
