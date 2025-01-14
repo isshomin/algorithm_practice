fa2e6e15d11fa9b217b72292a56a01f88bf33c8b
isshomin <79312705+isshomin@users.noreply.github.com>
2025-01-14 17:56:05 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    //answer = my_string.erase(s, my_string.size()-s);
+    //answer = my_string.append(overwrite_string);
+    answer = my_string.replace(s, overwrite_string.size(), overwrite_string);
+    return answer;
+}
