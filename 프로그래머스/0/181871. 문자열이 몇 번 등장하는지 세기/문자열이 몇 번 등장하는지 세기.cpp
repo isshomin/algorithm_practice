4da0b1ad8a430a95471c934380b4d36d0d94acac
isshomin <79312705+isshomin@users.noreply.github.com>
2025-01-14 17:51:22 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    size_t pos = 0;
+    while((pos = myString.find(pat, pos)) != string::npos)
+    {
+        answer++;
+        pos++;
+    }
+    return answer;
+}
