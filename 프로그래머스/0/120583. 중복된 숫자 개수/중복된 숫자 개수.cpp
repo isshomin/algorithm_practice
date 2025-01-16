1192669c6a21a5e3aea97bc435ddfcdf6f95c60a
isshomin <79312705+isshomin@users.noreply.github.com>
2025-01-16 16:02:13 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int n) {
+    int answer = 0;
+    for(const auto& i : array)
+        if(i == n)
+            answer++;
+    
+    return answer;
+}
