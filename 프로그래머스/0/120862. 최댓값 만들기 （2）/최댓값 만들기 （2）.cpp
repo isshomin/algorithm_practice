fcc6cd4af93553dfc21d7439da4d921a3b344d2e
isshomin <79312705+isshomin@users.noreply.github.com>
2025-01-16 16:07:09 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -7,9 +7,7 @@ int solution(vector<int> numbers) {
     int answer = 0;
     sort(numbers.begin(), numbers.end());
     
-    int max1 = numbers[0] * numbers[1];
-    int max2 = numbers[numbers.size() - 1] * numbers[numbers.size() - 2];
-    answer = max(max1, max2);
+    answer = max(numbers[0] * numbers[1], numbers[numbers.size() - 1] * numbers[numbers.size() - 2]);
         
     return answer;
 }
