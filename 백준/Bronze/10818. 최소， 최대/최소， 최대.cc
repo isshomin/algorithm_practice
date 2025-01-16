f9a7c11c60a2ad4b5987c6af2fd72903f1b683a5
isshomin <79312705+isshomin@users.noreply.github.com>
2025-01-16 22:27:10 +0900

    [Bronze III] Title: 최소, 최대, Time: 408 ms, Memory: 5800 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i < n; i++)
+        cin >> arr[i];
+    
+    int min = *min_element(arr, arr + n);
+    int max = *max_element(arr, arr + n);
+    
+    cout << min << " " << max;
+}
