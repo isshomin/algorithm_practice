2d3464323528e588f565a8d5d0944266c89b4840
isshomin <79312705+isshomin@users.noreply.github.com>
2025-02-24 15:40:12 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 long long solution(int a, int b) {
     long long answer = 0;
-    long long s = min(a, b);
-    long long l = max(a, b);
     
-    return (l - s + 1) * (l + s) / 2;
+    if(a > b)
+        swap(a, b);
+    
+    answer = 1LL * (b - a + 1) * (a + b) / 2;
+    return answer;
 }
